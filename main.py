860650147fe739785d94da90acf26d1b5c0fce1f
Everson Rubira <85372823+EversonRubira@users.noreply.github.com>
2026-05-20 11:49:05 +0000

    fix: ajutando autenticacao

--- main.py
+++ main.py
@@ -12,22 +12,21 @@ atividade = "musica"
 prompt = f"Crie um roteiro de viagem de {numero_dias}dias, para uma familia co {numero_criancas} criancas, que gosta de {atividade}"
 
 
-cliente = Anthropic(api_key=api_key)
+cliente = Anthropic()
 
 resposta = cliente.messages.create(
     model="claude-haiku-4-5",
     max_tokens = 1000,
+    system="Voce é uma assitente de roteiro de viagens.",
     messages=[
-        {
-            "role" : "system",
-            "content" : "Voce é uma assitente de roteiro de viagens."\
-        },
+       
         {
             "role" : "user",
             "content" : prompt
         }
     ]
 )
+
 
 resposta_em_texto = resposta.choice[0].message.content
 print(resposta)
