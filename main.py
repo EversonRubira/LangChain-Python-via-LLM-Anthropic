03143739a5154633f2da8b161b16afed5dc5921e
Everson Rubira <85372823+EversonRubira@users.noreply.github.com>
2026-05-21 13:48:53 +0000

    Atualizations

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-
-from anthropic import Anthropic
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 api_key = os.getenv("ANTHROPIC_API_KEY")
 
-numero_dias = 7
-numero_criancas = 2
-atividade = "musica" 
-
-prompt = f"Crie um roteiro de viagem de {numero_dias}dias, para uma familia co {numero_criancas} criancas, que gosta de {atividade}"
 
 
-cliente = Anthropic()
-
-resposta = cliente.messages.create(
-    model="claude-haiku-4-5",
-    max_tokens = 1000,
-    system="Voce é uma assitente de roteiro de viagens.",
-    messages=[
-       
-        {
-            "role" : "user",
-            "content" : prompt
-        }
-    ]
+modelo_cidade = ChatPromptTemplate.from_template(
+    template = """
+    Sugira uma cidade dado o seu interesse por {interesse}.
+    """,
+   
 )
 
-resposta = resposta.content[0].text
+modelo = ChatAnthropic(
+    model="claude-haiku-4-5",
+    temperature=0.7,
+    api_key=api_key
+)
+
+cadeia = modelo_cidade | modelo | StrOutputParser
+
+resposta = cadeia.invoke(
+    {
+        "interesse" : "praias"
+    }
+)
+
 print(resposta)
+
